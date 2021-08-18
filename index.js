a728ed3845080987754b9163c3453d24d892609e
unknown <chiennext@gmail.com>
2021-08-18 07:56:51 +0700

    chien commit form lay thong tin

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyYUjIh0gIdOrdu5EGQLJVqtSUGrBVtGqZnn1MFQloQhkxw9FusMt5wNZ_7SjMOIeZCNg/exec'
-const form1 = document.forms['contact-form-1']
-const form2 = document.forms['contact-form-2']
-
+let currentdate = new Date();
+let datetime = currentdate.getDate() + "/"
+    + (currentdate.getMonth() + 1) + "/"
+    + currentdate.getFullYear() + " @ "
+    + currentdate.getHours() + ":"
+    + currentdate.getMinutes() + ":"
+    + currentdate.getSeconds();
+let form1 = document.forms['contact-form-1']
+let form2 = document.forms['contact-form-2']
 form1.addEventListener('submit', e => {
+    form1['data[time]'].value = datetime;
     e.preventDefault()
-    fetch(scriptURL, {
-            method: 'POST',
-            body: new FormData(form1)
-        })
+    fetch(form1.action, {
+        method: "POST",
+        body: new FormData(form1)
+    })
         .then(response => {
             form1.reset();
+            alert("Đã gửi đăng ký thành công!")
             console.log('Success!', response);
         })
         .catch(error => console.error('Error!', error.message))
 })
+
 form2.addEventListener('submit', e => {
+    form2['data[time]'].value = datetime;
     e.preventDefault()
-    fetch(scriptURL, {
-            method: 'POST',
-            body: new FormData(form2)
-        })
+    fetch(form2.action, {
+        method: "POST",
+        body: new FormData(form2)
+    })
         .then(response => {
             form2.reset();
+            alert("Đã gửi đăng ký thành công!")
             console.log('Success!', response);
         })
         .catch(error => console.error('Error!', error.message))
