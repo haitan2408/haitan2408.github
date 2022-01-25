88e5a439d860d7ae6a2e5ebdbf5d77a91fa67885
unknown <chiennext@gmail.com>
2022-01-25 10:23:36 +0700

    chien update validate form

--- index.js
+++ index.js
@@ -8,18 +8,28 @@ let datetime = currentdate.getDate() + "/"
 let form1 = document.forms['contact-form-1']
 let form2 = document.forms['contact-form-2']
 form1.addEventListener('submit', e => {
-    form1['data[time]'].value = datetime;
-    e.preventDefault()
-    fetch(form1.action, {
-        method: "POST",
-        body: new FormData(form1)
-    })
-        .then(response => {
-            form1.reset();
-            alert("Đã gửi đăng ký thành công!")
-            console.log('Success!', response);
+    if (form1['data[name]'].value.trim().length == 0
+        || form1['data[email]'].value.trim().length == 0
+        || form1['data[phone]'].value.trim().length == 0) {
+        setTimeout(function () {
+            window.location = "http;://hoclaptrinh.codegym.vn/";
+        }, 1000)
+        alert("Vui lòng nhập đầy đủ thông tin!");
+    } else {
+        form1['data[time]'].value = datetime;
+        e.preventDefault()
+        fetch(form1.action, {
+            method: "POST",
+            body: new FormData(form1)
         })
-        .catch(error => console.error('Error!', error.message))
+            .then(response => {
+                form1.reset();
+                alert("Đã gửi đăng ký thành công!")
+                console.log('Success!', response);
+            })
+            .catch(error => console.error('Error!', error.message))
+    }
+
 })
 
 form2.addEventListener('submit', e => {
